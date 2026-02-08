4fc770d8857f18483312270ca3c5b447823fe1c1
shiyuan <654711746@qq.com>
2026-02-09 00:48:49 +0800

    section2.4

---
+++ chapter2/Section2/simple_messages.py
@@ -0,0 +1,6 @@
+msg="hello world"
+print(msg)
+
+
+msg="hello pycharm"
+print(msg)
---
+++ chapter2/Section3/diy_msg.py
@@ -0,0 +1,8 @@
+#%%
+firstname="song"
+lastname='bo'
+print(f"Hellow {firstname} {lastname} ,would you like to learn some Python today?")
+print(f"Hellow {firstname.lower()} {lastname.lower()} ,would you like to learn some Python today?")
+print(f"Hellow {firstname.upper()} {lastname.upper()} ,would you like to learn some Python today?")
+print(f"Hellow {firstname.title()} {lastname.title()} ,would you like to learn some Python today?")
+
